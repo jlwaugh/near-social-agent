dc61a14b461a2ba361e3ecfaea6bd84000782e71
jaswinder6991 <jassmultani2@gmail.com>
2024-09-18 14:03:10 +0530

    feat: adding dao search wip

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -2,6 +2,7 @@ import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { transactions, utils } from "near-api-js";
 import { fetchNonce, latestBlockHash } from "./utils";
+import axios from 'axios';
 
 const app = new Elysia({ prefix: "/api", aot: false })
   .use(swagger())
@@ -34,8 +35,6 @@ const app = new Elysia({ prefix: "/api", aot: false })
         ),
       );
 
-      //need nonce
-      //pass network from mbMetadata cause rpc node harcoded in latestblockhash
       const blockHash = await latestBlockHash();
       const nonce = await fetchNonce(accountId, publicKey);
       const transaction = transactions.createTransaction(
@@ -46,18 +45,23 @@ const app = new Elysia({ prefix: "/api", aot: false })
         actions,
         utils.serialize.base_decode(blockHash),
       );
-      console.log(transaction);
       return transaction;
-      // return transactions.createTransaction(
-      //   accountId,
-      //   publicKey,
-      //   dao,
-      //   nonce,
-      //   actions,
-      //   utils.serialize.base_decode(blockHash),
-      // );
     },
   )
+  .get("/daos/:accountId?", async ({ params, headers }) => {
+    const mbMetadata = JSON.parse(headers["mb-metadata"] || "{}");
+    const accountId = params["accountId?"] || mbMetadata?.accountData?.accountId || "near";
+
+    const apiKey = "29231aff-8c08-4f38-9096-b1d947050d27";
+    const response = await axios.get('https://api.pikespeak.ai/daos/members', {
+      headers: { 'x-api-key': apiKey }
+    });
+
+    const allDaos = response.data;
+    const userDaos = allDaos[accountId]?.daos || [];
+
+    return { daos: userDaos };
+  })
   .compile();
 
 export const GET = app.handle;
